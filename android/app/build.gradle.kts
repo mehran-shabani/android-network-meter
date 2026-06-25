03369fbfd96d523e44007d9f0fec5aca47ca31a9
Dr.hossein Shabani <shabanimehran@gmail.com>
2026-06-25 20:16:48 +0330

    Add Android app Gradle

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "ir.helssa.netmeter"
+    compileSdk = 37
+
+    defaultConfig {
+        applicationId = "ir.helssa.netmeter"
+        minSdk = 23
+        targetSdk = 37
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+flutter {
+    source = "../.."
+}
